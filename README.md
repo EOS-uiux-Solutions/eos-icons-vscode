d3a36b4d9f99c3da06583d04e2b492ab4e6eb0e2
Dan Čermák <dcermak@suse.com>
2021-02-26 11:34:23 +0100

    Initial prototype of the eos icons extension for VSCode

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# eos-icons-vsc
+# EOS Icons for VSCode
+
+This extension allows you to replace VSCode's default icons with those from the
+[EOS icon set](https://eos-icons.com/).
+
+
+## Usage
+
+Install the extension from the Marketplace, execute the command `Preferences:
+Product Icon Theme` and select the entry `EOS Icons` in the presented drop down:
+
+![select_product_theme.png](select_product_theme.png)
---
+++ create_icon_theme.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import shutil
+import json
+from os.path import dirname, join
+
+from fontTools.ttLib import TTFont
+
+dest = join(dirname(__file__), "./theme/eos-icons.woff")
+shutil.copyfile(
+    join(
+        dirname(__file__), "./node_modules/eos-icons/dist/fonts/eos-icons.woff"
+    ),
+    dest,
+)
+
+code_points = {}
+
+with TTFont(dest, 0, ignoreDecompileErrors=True) as ttf:
+    for x in ttf["cmap"].tables:
+        for (code, name) in x.cmap.items():
+            code_points[name] = code
+
+
+icon_to_glyph_mapping = [
+    ["accounts-view-bar-icon", "account_circle"],
+    ["callhierarchy-incoming", "call_received"],
+    ["callhierarchy-outgoing", "call_made"],
+    ["callstack-view-session", "bug_report"],
+    ["comments-view-icon", "comment"],
+    ["debug-configure", "settings_applications"],
+    ["extensions-manage", "settings_applications"],
+    ["settings-view-bar-icon", "settings_applications"],
+]
+
+eos_icons_theme = {
+    "fonts": [
+        {
+            "id": "eos-icons",
+            "src": [
+                {
+                    "path": "./eos-icons.woff",
+                    "format": "woff",
+                },
+            ],
+            "weight": "normal",
+            "style": "normal",
+        },
+    ],
+    "iconDefinitions": {},
+}
+
+for (icon_name, glyph_name) in icon_to_glyph_mapping:
+    eos_icons_theme["iconDefinitions"][icon_name] = {
+        "fontCharacter": chr(code_points[glyph_name]),
+    }
+
+with open(
+    join(dirname(__file__), "theme/eos-icons-product-icon-theme.json"), "w"
+) as product_icon_file:
+    product_icon_file.write(json.dumps(eos_icons_theme, indent=2))
